20ab4d31b23a62329f6a79da6c5e866bbc88d86e
joeloverbeck <joeloverbeck@gmail.com>
2022-01-18 22:20:55 +0100

    Created initial Bevy window.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_system(hello_world_system)
+        .insert_resource(WindowDescriptor {
+            title: "Procedural Exploration".to_string(),
+            width: 640.0,
+            height: 400.0,
+            vsync: true,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins)
         .run();
-}
-
-fn hello_world_system() {
-    println!("hello world");
 }
