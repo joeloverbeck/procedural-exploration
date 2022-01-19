797269ad4b64be4f7d8bdaa1f35f7f1a4c128053
joeloverbeck <joeloverbeck@gmail.com>
2022-01-19 18:26:56 +0100

    Added some stuff related to compilation.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use bevy::prelude::*;
+use std::{io, io::BufRead};
+
+struct Input(String);
+
+/// This example demonstrates you can create a custom runner (to update an app manually). It reads
+/// lines from stdin and prints them from within the ecs.
+fn my_runner(mut app: App) {
+    println!("The fuck you mean, boy?");
+    for line in io::stdin().lock().lines() {
+        {
+            let mut input = app.world.get_resource_mut::<Input>().unwrap();
+            input.0 = line.unwrap();
+        }
+        app.update();
+    }
+}
+
+fn print_system(input: Res<Input>) {
+    println!("What the hell do you mean by '{}'?!", input.0);
+}
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor {
-            title: "Procedural Exploration".to_string(),
-            width: 640.0,
-            height: 400.0,
-            vsync: true,
-            ..Default::default()
-        })
-        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
-        .add_plugins(DefaultPlugins)
+        .insert_resource(Input(String::new()))
+        .set_runner(my_runner)
+        .add_system(print_system)
         .run();
 }
